30f378cf7cce3b6b22776f4e63c798b1d8a2b52b
MiguelFernandes13 <miguelffernandes13@gmail.com>
2023-03-25 15:49:09 +0000

    [EA] - Add Platforms and User

--- ea/aa/GMS/hibernate_annotations/demo/src/main/java/com/example/App.java
+++ ea/aa/GMS/hibernate_annotations/demo/src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.FlushMode;
 import org.hibernate.Session;
@@ -19,7 +21,9 @@ public class App
         try{
             Configuration cfg = new Configuration()
                 .configure(new File("/home/miguel/uni/4ano/ea/aa/GMS/hibernate_annotations/demo/hibernate.cfg.xml"))
-                .addAnnotatedClass(Game.class);
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Game.class)
+                .addAnnotatedClass(Platform.class);
             StandardServiceRegistry sr = new StandardServiceRegistryBuilder()
                             .applySettings(cfg.getProperties()).build();
 
@@ -30,12 +34,43 @@ public class App
 
             Transaction t = s.beginTransaction();
 
-            Game g = new Game();
-            g.setName("GTA");
-            g.setYear(2010);
-            g.setPrice(1000);
-            g.setDescription("GTA is a game");
-            s.persist(g);
+            //Platform p = new Platform();
+            //p.setName("PS4");
+            //p.setYear(2010);
+            //p.setDescription("PS4 is a console");
+            //p.setManufacturer("Sony");
+            //s.persist(p);
+            Platform p = s.get(Platform.class, 1);
+
+            //Game g = new Game();
+            //g.setName("GTA");
+            //g.setYear(2010);
+            //g.setPrice(1000);
+            //g.setDescription("GTA is a game");
+            //g.setPlatform(p);
+            //s.persist(g);
+            Game g = s.get(Game.class, 1);
+
+            Game g2 = new Game();
+            g2.setName("GTA2");
+            g2.setYear(2010);
+            g2.setPrice(1000);
+            g2.setDescription("GTA is a game");
+            g2.setPlatform(p);
+            s.persist(g2);
+
+            List<Game> games = new ArrayList<>();
+            games.add(g);
+            games.add(g2);
+
+            User u = new User();
+            u.setName("Miguel");
+            u.setEmail("miguel@email.com");
+            u.setPassword("1234");
+            u.setGames(games);
+            s.persist(u);
+
+
 
             try{
                 t.commit();
--- ea/aa/GMS/hibernate_annotations/demo/src/main/java/com/example/Platform.java
+++ ea/aa/GMS/hibernate_annotations/demo/src/main/java/com/example/Platform.java
@@ -4,22 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 
 @Entity
-public class Game {
+public class Platform {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int year;
-    private double price;
     private String description;
-    @OneToOne
-    private Platform platform;
-
+    private String manufacturer;
     
-    public Game() {
+    
+    public Platform() {
     }
 
     public int getId() {
@@ -40,28 +37,19 @@ public class Game {
     public void setYear(int year) {
         this.year = year;
     }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
     public String getDescription() {
         return description;
     }
     public void setDescription(String description) {
         this.description = description;
     }
-
-    public Platform getPlatform() {
-        return platform;
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
     }
 
-    public void setPlatform(Platform platform) {
-        this.platform = platform;
-    }
-
-    
     
     
 }
